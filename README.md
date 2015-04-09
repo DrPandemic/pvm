6c4d479248313638e2cb7ee280eedb3a4e0dddee
DrPandemic <bipbip500@gmail.com>
2015-04-08 20:54:13 -0400

    * README + check for argv and did the help and ls

--- README.md
+++ README.md
@@ -3,10 +3,13 @@ Easily manage Python versions.
 
 # Goal
 There is already many solutions like : [virtualenv](https://pypi.python.org/pypi/virtualenv) and others. But there is none really simple to use.
-The inspiration comes from [nvm](https://github.com/creationix/nvm) and [rvm](https://github.com/wayneeseguin/rvm). 
+The inspiration comes from [nvm](https://github.com/creationix/nvm) and [rvm](https://github.com/wayneeseguin/rvm).
 The initiale goal is to have a limited set of functions, but just enough to be able to quickly run application with any version of Python on your system.
 
+# Good to know
+I'm desinging it to work on my arch linux box, so there is some specific things that could only apply for arch. If you are interested to help for other distro, leave an issue.
+
 # Usage
-```pvm list``` to get the list of installed versions.
+```pvm ls``` to get the list of installed versions.
 
 ```pvm use version``` to set this your python version.
--- pvm.py
+++ pvm.py
@@ -1,10 +1,44 @@
 import re
 from subprocess import check_output
+import sys
+
+acceptedCommands = [('help', 0),('ls', 0), ('use', 1), ('use', 0)]
 
 def byteToString (b): return b.decode('utf-8')
+def listVersions ():
+    pyFileRegex = re.compile('^python[0-9]\.?[0-9]?$').search
+    files = map(byteToString, check_output(['ls', '/usr/bin']).splitlines())
+    return filter(pyFileRegex, files)
+def printVersions ():
+    for count, version in enumerate(listVersions()):
+        print('(' + str(count) + ') ' + version)
 
-pyFileRegex = re.compile('python[0-9]\.?[0-9]?').search
-files = map(byteToString, check_output(["ls", "-al", "/usr/bin"]).splitlines())
-fFiles = filter(pyFileRegex, files)
-for a in fFiles:
-    print(a)
+def checkArgv ():
+    args = sys.argv
+    if len(args) == 1:
+        showHelp()
+        exit(0)
+
+    #Checks if the args are ok
+    isOk = False
+    for arg in acceptedCommands:
+        if arg[0] == args[1] and len(args) == arg[1] + 2:
+            isOk = True;
+
+    #Manage different args
+    if args[1] == 'help':
+        showHelp()
+        exit(0)
+    elif args[1] == 'ls':
+        print('\nYou currently have those Pythons installed (some may only be symlinks):\n')
+        printVersions()
+
+def showHelp ():
+    print('Python Version Manager\n')
+    print('Usage:')
+    print(' pvm help                Show this message')
+    print(' pvm list                List installed versions')
+    print(' pvm use                 Show installed versions and let you choose which one to set as default')
+    print(' pvm use version         Let you choose which version to use as default')
+
+checkArgv()
